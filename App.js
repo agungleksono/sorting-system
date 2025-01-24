c20a4ebe346578312fbf77d8ac53b6713367ebd0
agungleksono <agungleksono99@gmail.com>
2025-01-24 15:56:06 +0700

    changes App.tsx to App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color: 'red', textAlign: 'center'}}>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
